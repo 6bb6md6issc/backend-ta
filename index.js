e24ca1eca101fb056a3c721829ac0f3c72337a77
“Kaiwen <kevin9966hu@gmail.com>
2024-12-23 23:24:42 -0800

    Update CORS configuration

--- index.js
+++ index.js
@@ -8,12 +8,26 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 connectToDatabase();
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://3.15.26.182:5001',
+    'http://3.15.26.182'
+];
+
 app.use(cors({
-    origin: 'http://localhost:3000', // frontend URL
-    credentials: true,  // important for cookies
+    origin: function(origin, callback) {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            return callback(new Error('CORS policy: Origin not allowed'), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use('/api/auth', Route);
 
 app.listen(5001, () => {
